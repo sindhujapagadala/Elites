bb5a13385fafbc3fc3d880c5d2b4752d30e26fe9
AbhinavNeema <abhinavneema22gmail.com>
2025-04-28 13:47:21 +0530

    song page changes

---
+++ EchoMix/src/components/Aboutus/song/Song.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './song.css';
+import CustomNavbar from '../Navbar';
+import SongInfo from '../SongInfo';
+import Player from '../Player';
+
+function Song() {
+  return (
+    <div className="App" style={{ minHeight: '100vh' }}>
+      <CustomNavbar />
+      <main style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        minHeight: 'calc(100vh - 150px)',
+        paddingBottom: '4rem'
+      }}>
+        <SongInfo />
+        <Player />
+      </main>
+    </div>
+  );
+}
+
+export default Song;
---
+++ EchoMix/src/components/Aboutus/song/SongInfo.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import songImage from '../songphoto/meremehbob.jpeg';
+
+const SongInfo = () => {
+  return (
+    <div style={{ 
+      width: "min(90vw, 400px)",
+      height: "auto",
+      textAlign: "center",
+      margin: "2rem auto 0",
+      color: "antiquewhite",
+      padding: "0 1rem"
+    }}>
+      <img 
+        src={songImage} 
+        alt="song" 
+        style={{ 
+          width: "100%",
+          height: "auto",
+          maxHeight: "400px",
+          maxWidth: "400px",
+          borderRadius: "30px",
+          aspectRatio: "1/1",
+          objectFit: "cover"
+        }}
+      />
+      <h3 style={{ fontSize: "clamp(1.2rem, 4vw, 1.5rem)", marginTop: "1rem" }}>
+        Mere Mehboob Mere Sanam
+      </h3>
+    </div>
+  );
+};
+
+export default SongInfo;
